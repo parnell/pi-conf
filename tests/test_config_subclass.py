6f8c75ba90961b8d18c5c817d4b2a1c5dbf4cba5
parnell <3028114+parnell@users.noreply.github.com>
2023-12-20 12:38:52 -0600

    Update test_config_subclass.py

--- tests/test_config_subclass.py
+++ tests/test_config_subclass.py
@@ -2,7 +2,7 @@ import os
 import sys
 import unittest
 
-from pi_conf import Config
+from pi_conf import AttrDict, Config
 
 basedir = os.path.abspath(os.getcwd())
 sys.path.append(basedir)
@@ -36,11 +36,19 @@ class TestSubclass(unittest.TestCase):
         class MyConfig(Config):
             a: int = 1
 
-        cfg = MyConfig.from_dict({"a": {"b": 1}})
+        cfg = MyConfig.from_dict({"a": {"b": 1}}, _nested_same_class=True)
         self.assertEqual(cfg.a.b, 1)
         self.assertEqual(type(cfg), MyConfig)
         self.assertEqual(type(cfg.a), MyConfig)
 
+    def test_dataclass_subclass_from_dict_recursive_nested_false(self):
+        class MyConfig(Config):
+            a: int = 1
+
+        cfg = MyConfig.from_dict({"a": {"b": 1}}, _nested_same_class=False)
+        self.assertEqual(cfg.a.b, 1)
+        self.assertEqual(type(cfg), MyConfig)
+        self.assertEqual(type(cfg.a), AttrDict)
 
 
 if __name__ == "__main__":
