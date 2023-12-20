0798b4c619edc1709fd5f465abc6a6bc75a4e5c0
parnell <3028114+parnell@users.noreply.github.com>
2023-12-20 12:38:20 -0600

    Update __init__.py

--- pi_conf/__init__.py
+++ pi_conf/__init__.py
@@ -1,3 +1,4 @@
+from pi_conf.config import AttrDict as AttrDict
 from pi_conf.config import Config as Config
 from pi_conf.config import cfg as cfg
 from pi_conf.config import load_config as load_config
